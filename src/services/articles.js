c956fb91a0c52808f0ec22fd2e00451772d7268a
Radu Soltan <radusoltan@gmail.com>
2024-04-05 23:35:36 +0200

    - Article updated form; - Article update (with notification);

--- src/services/articles.js
+++ src/services/articles.js
@@ -15,8 +15,12 @@ export const articles = createApi({
   }),
   tagTypes: ["Articles"],
   endpoints: build => ({
+    getArticle: build.query({
+      query: ({article, locale}) => `/articles/${article}?locale=${locale}`,
+      providesTags: result => [{type: "Articles", id: result.id}],
+    }),
     getCategoryArticles: build.query({
-      query: ({page, category, locale})=> `/category/${category}/articles?page=${page}?locale=${locale}`,
+      query: ({page, category, locale})=> `/category/${category}/articles?page=${page}&locale=${locale}`,
       providesTags: result => [
           ...result.data.map(({id})=>({ type: "Articles", id })),
         { type: "Articles", id: "PARTIAL-LIST" }
@@ -27,12 +31,29 @@ export const articles = createApi({
         url: `/category/${category}/article?locale=${locale}`,
         method: "POST",
         body
-      })
+      }),
+      invalidatesTags: response => [
+        { type: "Articles", id: response.data.id },
+        { type: "Articles", id: "PARTIAL-LIST" }
+      ]
+    }),
+    updateArticle: build.mutation({
+      query: ({article, locale, body}) => ({
+        url: `/articles/${article}?locale=${locale}`,
+        method: "PATCH",
+        body
+      }),
+      invalidatesTags: result => [
+        {type: "Articles", id: result.data.id},
+        { type: "Articles", id: "PARTIAL-LIST" }
+      ]
     })
   })
 })
 
 export const {
+  useGetArticleQuery,
   useGetCategoryArticlesQuery,
   useAddCategoryArticleMutation,
+  useUpdateArticleMutation,
 } = articles
