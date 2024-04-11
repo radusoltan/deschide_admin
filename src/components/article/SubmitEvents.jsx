eee88541f85b140ddde1b1a2bbdee0f3a99ea5c9
Radu Soltan <radusoltan@gmail.com>
2024-04-11 22:13:00 +0200

    - set and delete Article publish time;

---
+++ src/components/article/SubmitEvents.jsx
@@ -0,0 +1,66 @@
+import {Button, Card, DatePicker, Space} from "antd"
+import moment from "moment"
+import {CheckCircleOutlined} from "@ant-design/icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrashCan} from "@fortawesome/free-solid-svg-icons";
+import {useDeleteArticlePublishTimeMutation, useSetArticlePublishTimeMutation} from "../../services/articles";
+import {useEffect, useState} from "react";
+
+export const SubmitEvents = ({ article, publish_at }) =>{
+
+
+
+  const [setArticlePublishTime, {isSuccess: setIsSuccess, data: setData}] = useSetArticlePublishTimeMutation()
+  const [deletePublishEvent, {isSuccess: deleteSuccess, data: deleteData}] = useDeleteArticlePublishTimeMutation()
+
+  const [publishTime, setPublishTime] = useState()
+  const date = moment(publish_at)
+
+  console.log(publish_at)
+  // useEffect(() => {
+  //   // if (setIsSuccess){
+  //   //   setPublishTime(setData.publish_at)
+  //   // }
+  //   // if (deleteData){
+  //   //   setPublishTime(deleteData.publish_at)
+  //   // }
+  //   console.log(publish_at)
+  // }, [setIsSuccess, deleteSuccess,publish_at]);
+
+  return <Card title="Select publish time" style={{
+    marginTop: 25
+  }}>
+    {publishTime && <Space direction="vertical">
+      <div style={{marginBottom: 20}}>
+        <div style={{ padding: 5, border: '1px solid #e9e9e9' }}>
+          {date.format('MMM DD YYYY kk:mm')}
+          <Button
+              type="primary"
+              danger
+              icon={<FontAwesomeIcon icon={faTrashCan} />}
+              onClick={()=>deletePublishEvent(article)}
+              style={{
+                marginLeft: 50
+              }}
+          />
+        </div>
+      </div>
+    </Space>}
+    <Space direction="vertical">
+      <DatePicker
+          showTime
+          onChange={()=>{
+
+          }}
+          onOk={date=>{
+            setArticlePublishTime({
+              article,
+              body: {
+                time: date.format()
+              }
+            })
+          }}
+      />
+    </Space>
+  </Card>
+}
